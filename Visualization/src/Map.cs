d5e212346736e5a116e5f7ef07a7ccfd25216872
antoni <reym@veril.mozmail.com>
2025-05-25 20:06:33 +0200

    fix map drawing logic

--- Visualization/src/Map.cs
+++ Visualization/src/Map.cs
@@ -1,18 +1,54 @@
+using System.Diagnostics;
 using Godot;
 
 namespace mmvp.src;
 
+
 public class Map(List<List<Map.Field>> data)
 {
-    private readonly Vector2I? wall = new(12, 5);
-    private readonly Vector2I? wallCorner = new(12, 4);
-    private readonly Vector2I? floor = null;
-    private readonly Vector2I? hill = new(6, 0);
-    private readonly Vector2I? ditch = new(7, 4);
-    private readonly Vector2I? water = new(7, 4);
-    private readonly Vector2I? explosiveBarrel = new(7, 4);
-    private readonly Vector2I? flagStand1 = new(7, 4);
-    private readonly Vector2I? flagStand2 = new(7, 4);
+    private static TileSetCoordinates GetScribbleDungeonTiles()
+    {
+
+        return new TileSetCoordinates(
+            wall: new(new(5, 5)),
+            wallCorner: new(new(5, 4)),
+            floor: new(new(0, 0)),
+            hill: new(new(5, 0)),
+            ditch: new(new(0, 0), 1),
+            water: new(new(0, 0), 2),
+            explosiveBarrel: new(new(5, 2)),
+            flagStand1: new(new(1, 8)),
+            flagStand2: new(new(3, 8))
+        );
+    }
+    
+    private class TileSetCoordinates(
+        TileMapCell wall,
+        TileMapCell wallCorner,
+        TileMapCell floor,
+        TileMapCell hill,
+        TileMapCell ditch,
+        TileMapCell water,
+        TileMapCell explosiveBarrel,
+        TileMapCell flagStand1,
+        TileMapCell flagStand2)
+    {
+        public readonly TileMapCell Wall = wall;
+        public readonly TileMapCell WallCorner = wallCorner;
+        public readonly TileMapCell Floor = floor;
+        public readonly TileMapCell Hill = hill;
+        public readonly TileMapCell Ditch = ditch;
+        public readonly TileMapCell Water = water;
+        public readonly TileMapCell ExplosiveBarrel = explosiveBarrel;
+        public readonly TileMapCell FlagStand1 = flagStand1;
+        public readonly TileMapCell FlagStand2 = flagStand2;
+    }
+
+    private class TileMapCell(Vector2I atlasCoords, int alternativeTile = 0)
+    {
+        public readonly Vector2I AtlasCoords = atlasCoords;
+        public readonly int AlternativeTile = alternativeTile;
+    }
 
     public enum Field
     {
@@ -26,19 +62,15 @@ public class Map(List<List<Map.Field>> data)
         FlagStand2,
     }
 
-    private readonly List<List<Field>> data = data;
+    private readonly TileSetCoordinates currentTileSetCoords = GetScribbleDungeonTiles();
 
     public static Map ReadInMap(string filename)
     {
         var mapData = new List<List<Field>>();
-
         string[] lines = File.ReadAllLines(filename);
-
         foreach (string line in lines)
         {
-            var row = line
-                .Split(';')
-                .Select(field => field switch
+            var row = line.Split(';').Select(field => field switch
             {
                 "0" => Field.Floor,
                 "1" => Field.Wall,
@@ -49,26 +81,23 @@ public class Map(List<List<Map.Field>> data)
                 "7" => Field.FlagStand1,
                 "8" => Field.FlagStand2,
                 _ => throw new ArgumentException("Encountered an unknown map field."),
-            })
-            .ToList();
+            }).ToList();
             mapData.Add(row);
         }
+
         return new Map(mapData);
     }
 
     public override string ToString()
     {
-        return string.Join("",
-                data.ConvertAll(row => string.Join("",
-                        row.ConvertAll(field => field switch
-                        {
-                            Field.Floor => " ",
-                            Field.Wall => "H",
-                            Field.Hill => "^",
-                            Field.Ditch => "_",
-                            _ => "="
-                        }))
-                    + "\n"));
+        return string.Join("", data.ConvertAll(row => string.Join("", row.ConvertAll(field => field switch
+        {
+            Field.Floor => " ",
+            Field.Wall => "H",
+            Field.Hill => "^",
+            Field.Ditch => "_",
+            _ => "="
+        })) + "\n"));
     }
 
     public void PopulateTileMap(TileMapLayer tileMapLayer)
@@ -78,24 +107,49 @@ public class Map(List<List<Map.Field>> data)
             for (int x = 0; x < data[y].Count; ++x)
             {
                 var fieldValue = data[y][x];
-                Vector2I? atlasCoords = fieldValue switch
+                var tileSetField = fieldValue switch
                 {
-                    Field.Floor => floor,
-                    Field.Wall => wall,
-                    Field.Hill => hill,
-                    Field.Ditch => ditch,
-                    Field.Water => water,
-                    Field.ExplosiveBarrel => explosiveBarrel,
-                    Field.FlagStand1 => flagStand1,
-                    Field.FlagStand2 => flagStand2,
+                    Field.Floor => currentTileSetCoords.Floor,
+                    Field.Wall => currentTileSetCoords.Wall,
+                    Field.Hill => currentTileSetCoords.Hill,
+                    Field.Ditch => currentTileSetCoords.Ditch,
+                    Field.Water => currentTileSetCoords.Water,
+                    Field.ExplosiveBarrel => currentTileSetCoords.ExplosiveBarrel,
+                    Field.FlagStand1 => currentTileSetCoords.FlagStand1,
+                    Field.FlagStand2 => currentTileSetCoords.FlagStand2,
                     _ => throw new NotImplementedException(),
                 };
-                tileMapLayer.SetCell(new Vector2I(x, y), 3, atlasCoords);
+                if (fieldValue is not Field.Wall)
+                    tileMapLayer.SetCell(new Vector2I(x, y), 3, tileSetField.AtlasCoords);
+                else
+                    switch (GetWallPosition(x, y))
+                    {
+                        case WallPosition.Vertical:
+                            tileMapLayer.SetCell(new Vector2I(x, y), 3, tileSetField.AtlasCoords);
+                            break;
+                        case WallPosition.Horizontal:
+                            tileMapLayer.SetCell(new Vector2I(x, y), 3, tileSetField.AtlasCoords, 1);
+                            break;
+                        case WallPosition.LeftUpperCorner:
+                            tileMapLayer.SetCell(new Vector2I(x, y), 3, currentTileSetCoords.WallCorner.AtlasCoords);
+                            break;
+                        case WallPosition.RightUpperCorner:
+                            tileMapLayer.SetCell(new Vector2I(x, y), 3, currentTileSetCoords.WallCorner.AtlasCoords, 1);
+                            break;
+                        case WallPosition.LeftLowerCorner:
+                            tileMapLayer.SetCell(new Vector2I(x, y), 3, currentTileSetCoords.WallCorner.AtlasCoords, 2);
+                            break;
+                        case WallPosition.RightLowerCorner:
+                            tileMapLayer.SetCell(new Vector2I(x, y), 3, currentTileSetCoords.WallCorner.AtlasCoords, 3);
+                            break;
+                        default:
+                            throw new UnreachableException();
+                    }
             }
         }
     }
 
-    enum WallPosition
+    private enum WallPosition
     {
         Horizontal,
         Vertical,
@@ -118,10 +172,11 @@ public class Map(List<List<Map.Field>> data)
         return wallCount switch
         {
             1 => WallPosition.Horizontal,
-            2 when hasNorth && hasEast => WallPosition.LeftUpperCorner,
-            2 when hasNorth && hasEast => WallPosition.RightUpperCorner,
+            2 when hasNorth && hasSouth => WallPosition.Vertical,
+            2 when hasSouth && hasEast => WallPosition.LeftUpperCorner,
+            2 when hasSouth && hasWest => WallPosition.RightUpperCorner,
             2 when hasNorth && hasEast => WallPosition.LeftLowerCorner,
-            2 when hasNorth && hasEast => WallPosition.RightLowerCorner,
+            2 when hasNorth && hasWest => WallPosition.RightLowerCorner,
             _ => WallPosition.Horizontal,
 
         };
@@ -133,7 +188,7 @@ public class Map(List<List<Map.Field>> data)
         if (y < 0 || y >= data.Count || x < 0 || x >= data[y].Count)
             return false;
 
-        return data[y][x] == Field.Wall;
+        return data[y][x] is Field.Wall;
     }
 }
 
